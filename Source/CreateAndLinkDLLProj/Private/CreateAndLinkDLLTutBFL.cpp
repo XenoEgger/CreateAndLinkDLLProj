9ef3e2e8fbc2ac4dc4705e531ccfcf06e3b3d818
Benjamin W <XenoEgger@gmail.com>
2016-11-25 19:54:06 +0100

    Adds bool and int methods.
    Marks string method as buggy.

--- Source/CreateAndLinkDLLProj/Private/CreateAndLinkDLLTutBFL.cpp
+++ Source/CreateAndLinkDLLProj/Private/CreateAndLinkDLLTutBFL.cpp
@@ -3,13 +3,21 @@
 #include "CreateAndLinkDLLTutBFL.h"
 #include <string>
 
+
+typedef bool(*_getInvertedBool)(bool boolState); // Declare a method to store the DLL method getInvertedBool.
+typedef int(*_getIntPlusPlus)(int lastInt); // Declare a method to store the DLL method getIntPlusPlus.
 typedef float(*_getCircleArea)(float radius); // Declare a method to store the DLL method getCircleArea.
 typedef std::string(*_getAdditionalString)(std::string baseString); // Declare a method to store the DLL method getAdditionalString.
 
+_getInvertedBool m_getInvertedBoolFromDll;
+_getIntPlusPlus m_getIntPlusPlusFromDll;
 _getCircleArea m_getCircleAreaFromDll;
 _getAdditionalString m_getAdditionalStringFromDll;
 
 void *v_dllHandle;
+
+
+#pragma region Load DLL
 
 // Method to import a DLL.
 bool UCreateAndLinkDLLTutBFL::importDLL(FString folder, FString name)
@@ -26,8 +34,43 @@ bool UCreateAndLinkDLLTutBFL::importDLL(FString folder, FString name)
 	}
 	return false;	// Return an error.
 }
+#pragma endregion Load DLL
 
-// Method to import the method getCircleArea from the DLL.
+#pragma region Import Methods
+
+// Imports the method getInvertedBool from the DLL.
+bool UCreateAndLinkDLLTutBFL::importMethodGetInvertedBool()
+{
+	if (v_dllHandle != NULL)
+	{
+		m_getInvertedBoolFromDll = NULL;
+		FString procName = "getInvertedBool";	// Needs to be the exact name of the DLL method.
+		m_getInvertedBoolFromDll = (_getInvertedBool)FPlatformProcess::GetDllExport(v_dllHandle, *procName);
+		if (m_getInvertedBoolFromDll != NULL)
+		{
+			return true;
+		}
+	}
+	return false;	// Return an error.
+}
+
+// Imports the method getIntPlusPlus from the DLL.
+bool UCreateAndLinkDLLTutBFL::importMethodGetIntPlusPlus()
+{
+	if (v_dllHandle != NULL)
+	{
+		m_getIntPlusPlusFromDll = NULL;
+		FString procName = "getIntPlusPlus";	// Needs to be the exact name of the DLL method.
+		m_getIntPlusPlusFromDll = (_getIntPlusPlus)FPlatformProcess::GetDllExport(v_dllHandle, *procName);
+		if (m_getIntPlusPlusFromDll != NULL)
+		{
+			return true;
+		}
+	}
+	return false;	// Return an error.
+}
+
+// Imports the method getCircleArea from the DLL.
 bool UCreateAndLinkDLLTutBFL::importMethodGetCircleArea()
 {
 	if (v_dllHandle != NULL)
@@ -43,7 +86,7 @@ bool UCreateAndLinkDLLTutBFL::importMethodGetCircleArea()
 	return false;	// Return an error.
 }
 
-// Method to import the method getAdditionalString from the DLL.
+// Imports the method getAdditionalString from the DLL.
 bool UCreateAndLinkDLLTutBFL::importMethodGetAdditionalString()
 {
 	if (v_dllHandle != NULL)
@@ -58,8 +101,33 @@ bool UCreateAndLinkDLLTutBFL::importMethodGetAdditionalString()
 	}
 	return false;	// Return an error.
 }
+#pragma endregion Import Methods
 
-// Method to call the method m_getCircleAreaFromDll that was imported from the DLL.
+#pragma region Method Calls
+
+// Calls the method m_getCircleAreaFromDll that was imported from the DLL.
+bool UCreateAndLinkDLLTutBFL::getInvertedBoolFromDll(bool boolState)
+{
+	if (m_getCircleAreaFromDll != NULL)
+	{
+		bool out = bool(m_getInvertedBoolFromDll(boolState)); // Call the DLL method with arguments corresponding to the exact signature and return type of the method.
+		return out;
+	}
+	return boolState;	// Return an error.
+}
+
+// Calls the method m_getIntPlusPlusFromDll that was imported from the DLL.
+int UCreateAndLinkDLLTutBFL::getIntPlusPlusFromDll(int lastInt)
+{
+	if (m_getIntPlusPlusFromDll != NULL)
+	{
+		int out = int(m_getIntPlusPlusFromDll(lastInt)); // Call the DLL method with arguments corresponding to the exact signature and return type of the method.
+		return out;
+	}
+	return lastInt;	// Return an error.
+}
+
+// Calls the method m_getCircleAreaFromDll that was imported from the DLL.
 float UCreateAndLinkDLLTutBFL::getCircleAreaFromDll(float radius)
 {
 	if (m_getCircleAreaFromDll != NULL)
@@ -70,7 +138,8 @@ float UCreateAndLinkDLLTutBFL::getCircleAreaFromDll(float radius)
 	return -1.00f;	// Return an error.
 }
 
-// Method to call the method m_getAdditionalStringFromDll that was imported from the DLL.
+// BUGGY: Calls the method m_getAdditionalStringFromDll that was imported from the DLL.
+// To send the receive the origial baseString variable value seems to work all the time. Changes of the return value in the DLL causes the crashes.
 FString UCreateAndLinkDLLTutBFL::getAdditionalStringFromDll(FString baseString)
 {
 	if (m_getAdditionalStringFromDll != NULL)
@@ -82,6 +151,9 @@ FString UCreateAndLinkDLLTutBFL::getAdditionalStringFromDll(FString baseString)
 	}
 	return "Error: Method was probabey not imported yet!";	// Return an error.
 }
+#pragma endregion Method Calls
+
+#pragma region Unload DLL
 
 // If you love something  set it free.
 void UCreateAndLinkDLLTutBFL::freeDLL()
@@ -89,7 +161,8 @@ void UCreateAndLinkDLLTutBFL::freeDLL()
 	if (v_dllHandle != NULL)
 	{
 		FPlatformProcess::FreeDllHandle(v_dllHandle);
+		v_dllHandle = NULL;
 	}
 }
-
+#pragma endregion
 
--- Source/CreateAndLinkDLLProj/Public/CreateAndLinkDLLTutBFL.h
+++ Source/CreateAndLinkDLLProj/Public/CreateAndLinkDLLTutBFL.h
@@ -15,18 +15,38 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
 	static bool importDLL( FString folder, FString name);
 
-	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
-	static bool importMethodGetCircleArea();
+
 
 	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
+	static bool importMethodGetInvertedBool();
+
+	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
+	static bool importMethodGetIntPlusPlus();
+
+	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
+	static bool importMethodGetCircleArea();
+	
+	//buggy
+	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
 	static bool importMethodGetAdditionalString();
+
+
+
+	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
+	static bool getInvertedBoolFromDll(bool boolState);
+
+	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
+	static int getIntPlusPlusFromDll(int lastInt);
 
 	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
 	static float getCircleAreaFromDll(float radius);
 
+	//buggy
 	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
 	static FString getAdditionalStringFromDll(FString baseString);
 	
+
+
 	UFUNCTION(BlueprintCallable, Category = "My DLL Library")
 	static void freeDLL();
 };
